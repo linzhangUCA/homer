f9dbaae984c5c97dab798b4fb80ee74d40b427db
linzhanguca <lzhang12@uca.edu>
2023-08-10 21:33:53 -0500

    add control package

---
+++ homer_control/setup.py
@@ -0,0 +1,29 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'homer_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join("launch", "*.py"))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='linzhanguca',
+    maintainer_email='lzhang12@uca.edu',
+    description='Control HomeR velocity and estimate odometry',
+    license='GPL3',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'odometry = homer_control.odometry:main'
+        ],
+    },
+)
