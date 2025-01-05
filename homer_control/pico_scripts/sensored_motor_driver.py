8508d8c17080f7a10d0d862c9adae621d6cf1e58
Lin Zhang <linzhank@gmail.com>
2025-01-05 17:49:25 -0600

    update sensored motor test

--- homer_control/pico_scripts/sensored_motor_driver.py
+++ homer_control/pico_scripts/sensored_motor_driver.py
@@ -25,10 +25,11 @@ class SensoredMotorDriver(MotorDriver):
 if __name__ == '__main__':
     from time import sleep
     from math import pi
-    m = SensoredMotorDriver((11, 12, 13), (6, 7))
+    # m = SensoredMotorDriver((11, 12, 13), (14, 15))
+    m = SensoredMotorDriver((18, 19, 20), (17, 16))
     prev_counts = 0
 
-    # Following computation can be wrapped in a Timer callback
+    # Forward
     for d in range(200):  # ramp up
         m.forward(int(65025 / 200 * d))
         sleep(0.02)
@@ -36,24 +37,48 @@ if __name__ == '__main__':
         prev_counts = m.counts
         revs_inc = pulses_inc / m.PPR
         rads_inc = revs_inc * 2 * pi
-        ang_vel = rads_inc / 0.02  # motor angular velocity
-        # ang_vel_wheel = ang_vel_motor / smd.GEAR_RATIO  # wheel angular velocity
-        print(f"motor angular velocity: {ang_vel_wheel} rad/s")
-        # lin_vel = ang_vel_wheel * smd.WHEEL_RADIUS
-        # print(f"wheel linear velocity: {lin_vel} m/s")
-        
+        ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
+        ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
+        print(f"wheel angular velocity: {ang_vel_whl} rad/s")
+        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
+        # print(f"wheel linear velocity: {lin_vel} m/s")        
     for d in reversed(range(200)):  # ramp down
-        smd.forward(int(65025 / 200 * d))
+        m.forward(int(65025 / 200 * d))
         sleep(0.02)
-        pulses_inc = smd.counts - prev_counts
-        prev_counts = smd.counts
-        revs_inc = pulses_inc / smd.PPR
+        pulses_inc = m.counts - prev_counts
+        prev_counts = m.counts
+        revs_inc = pulses_inc / m.PPR
         rads_inc = revs_inc * 2 * pi
-        ang_vel_motor = rads_inc / 0.02  # motor angular velocity
-        ang_vel_wheel = ang_vel_motor / smd.GEAR_RATIO  # wheel angular velocity
-        print(f"wheel angular velocity: {ang_vel_wheel} rad/s")
-        lin_vel = ang_vel_wheel * smd.WHEEL_RADIUS
-        print(f"wheel linear velocity: {lin_vel} m/s")
+        ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
+        ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
+        print(f"wheel angular velocity: {ang_vel_whl} rad/s")
+        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
+        # print(f"wheel linear velocity: {lin_vel} m/s")
+    # Reverse
+    for d in range(200):  # ramp up
+        m.backward(int(65025 / 200 * d))
+        sleep(0.02)
+        pulses_inc = m.counts - prev_counts
+        prev_counts = m.counts
+        revs_inc = pulses_inc / m.PPR
+        rads_inc = revs_inc * 2 * pi
+        ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
+        ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
+        print(f"wheel angular velocity: {ang_vel_whl} rad/s")
+        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
+        # print(f"wheel linear velocity: {lin_vel} m/s")        
+    for d in reversed(range(200)):  # ramp down
+        m.backward(int(65025 / 200 * d))
+        sleep(0.02)
+        pulses_inc = m.counts - prev_counts
+        prev_counts = m.counts
+        revs_inc = pulses_inc / m.PPR
+        rads_inc = revs_inc * 2 * pi
+        ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
+        ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
+        print(f"wheel angular velocity: {ang_vel_whl} rad/s")
+        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
+        # print(f"wheel linear velocity: {lin_vel} m/s")
 
-    smd.stop()
+    m.stop()
     
