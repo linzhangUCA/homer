8c8db428f72173e1bfed2ec2e08bfa4f66c3ba27
linzhanguca <lzhang12@uca.edu>
2025-01-05 18:19:24 -0600

    add wheel driver with vel monitor

--- homer_control/pico_scripts/sensored_motor_driver.py
+++ homer_control/pico_scripts/sensored_motor_driver.py
@@ -8,33 +8,33 @@ class SensoredMotorDriver(MotorDriver):
         super().__init__(*driver_ids)  # call super class's "__init__"
         self.enca_pin = Pin(encoder_ids[0], Pin.IN, Pin.PULL_UP)  # yellow  
         self.encb_pin = Pin(encoder_ids[1], Pin.IN, Pin.PULL_UP)  # white
-        self.enca_pin.irq(trigger=Pin.IRQ_RISING, handler=self.update_counts)
+        self.enca_pin.irq(trigger=Pin.IRQ_RISING, handler=self.update_pulses)
         # Variables
-        self.counts = 0
+        self.pulses = 0
         # Properties
         self.PPR = 12  # pulses per revolution, PPR * 4 = CPR
         self.GEAR_RATIO = 46.8512  # speed reduction rate, v_wheel = v_motor / GEAR_RATIO
         # self.WHEEL_RADIUS = 0.032  # diameter in mm -> radius in m  
     
-    def update_counts(self, pin):
+    def update_pulses(self, pin):
         if self.encb_pin.value() == self.enca_pin.value():  # A channel RISE later than B channel
-            self.counts -= 1
+            self.pulses -= 1
         else:
-            self.counts += 1
+            self.pulses += 1
         
 if __name__ == '__main__':
     from time import sleep
     from math import pi
-    # m = SensoredMotorDriver((11, 12, 13), (14, 15))
-    m = SensoredMotorDriver((18, 19, 20), (17, 16))
-    prev_counts = 0
+    m = SensoredMotorDriver((11, 12, 13), (14, 15))
+    # m = SensoredMotorDriver((18, 19, 20), (17, 16))
+    prev_pulses = 0
 
     # Forward
     for d in range(200):  # ramp up
         m.forward(int(65025 / 200 * d))
         sleep(0.02)
-        pulses_inc = m.counts - prev_counts
-        prev_counts = m.counts
+        pulses_inc = m.pulses - prev_pulses
+        prev_pulses = m.pulses
         revs_inc = pulses_inc / m.PPR
         rads_inc = revs_inc * 2 * pi
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
@@ -45,8 +45,8 @@ if __name__ == '__main__':
     for d in reversed(range(200)):  # ramp down
         m.forward(int(65025 / 200 * d))
         sleep(0.02)
-        pulses_inc = m.counts - prev_counts
-        prev_counts = m.counts
+        pulses_inc = m.pulses - prev_pulses
+        prev_pulses = m.pulses
         revs_inc = pulses_inc / m.PPR
         rads_inc = revs_inc * 2 * pi
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
@@ -58,8 +58,8 @@ if __name__ == '__main__':
     for d in range(200):  # ramp up
         m.backward(int(65025 / 200 * d))
         sleep(0.02)
-        pulses_inc = m.counts - prev_counts
-        prev_counts = m.counts
+        pulses_inc = m.pulses - prev_pulses
+        prev_pulses = m.pulses
         revs_inc = pulses_inc / m.PPR
         rads_inc = revs_inc * 2 * pi
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
@@ -70,8 +70,8 @@ if __name__ == '__main__':
     for d in reversed(range(200)):  # ramp down
         m.backward(int(65025 / 200 * d))
         sleep(0.02)
-        pulses_inc = m.counts - prev_counts
-        prev_counts = m.counts
+        pulses_inc = m.pulses - prev_pulses
+        prev_pulses = m.pulses
         revs_inc = pulses_inc / m.PPR
         rads_inc = revs_inc * 2 * pi
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
---
+++ homer_control/pico_scripts/wheel_driver.py
@@ -0,0 +1,52 @@
+from sensored_motor_driver import SensoredMotorDriver
+from machine import Timer
+from math import pi
+
+class WheelDriver(SensoredMotorDriver):
+    
+    def __init__(self, driver_ids, encoder_ids):
+        # Pin configuration
+        super().__init__(driver_ids, encoder_ids)  # call super class's "__init__"
+        self.vel_mointor = Timer(mode=Timer.PERIODIC, freq=100, callback=self.compute_velocity)
+        # Variables
+        self.ang_vel = 0.
+        self.lin_vel = 0.
+        self.prev_pulses = 0
+        # Properties
+        self.WHEEL_RADIUS = 0.032  # diameter in mm -> radius in m  
+    
+    def compute_velocity(self, timer):
+        delta_pulses = self.pulses - self.prev_pulses
+        self.prev_pulses = self.pulses
+        delta_revolutions = delta_pulses / self.PPR
+        delta_radians = delta_revolutions * 2 * pi
+        ang_vel_mtr = delta_radians / 0.01  # compute velocity every 0.01 seconds
+        self.ang_vel = ang_vel_mtr / self.GEAR_RATIO
+        self.lin_vel = self.ang_vel * self.WHEEL_RADIUS
+
+if __name__ == '__main__':
+    from time import sleep
+    m = WheelDriver((11, 12, 13), (14, 15))
+    # m = WheelDriver((18, 19, 20), (17, 16))
+    prev_counts = 0
+
+    # Forward
+    for d in range(200):  # ramp up
+        m.forward(int(65025 / 200 * d))
+        sleep(0.02)
+        print(f"wheel velocity: \n\tangular: {m.ang_vel} rad/s, linear velocity: {m.lin_vel} m/s")
+    for d in reversed(range(200)):  # ramp down
+        m.forward(int(65025 / 200 * d))
+        sleep(0.02)
+        print(f"wheel velocity: \n\tangular: {m.ang_vel} rad/s, linear velocity: {m.lin_vel} m/s")
+    # Reverse
+    for d in range(200):  # ramp up
+        m.backward(int(65025 / 200 * d))
+        sleep(0.02)
+        print(f"wheel velocity: \n\tangular: {m.ang_vel} rad/s, linear velocity: {m.lin_vel} m/s")
+    for d in reversed(range(200)):  # ramp down
+        m.backward(int(65025 / 200 * d))
+        sleep(0.02)
+        print(f"wheel velocity: \n\tangular: {m.ang_vel} rad/s, linear velocity: {m.lin_vel} m/s")
+
+    m.stop()
