177bb010f531898faf4b7bfc80d08fb669acd47c
linzhanguca <lzhang12@uca.edu>
2025-01-06 17:16:05 -0600

    remove comments in test

--- homer_control/pico_scripts/sensored_motor_driver.py
+++ homer_control/pico_scripts/sensored_motor_driver.py
@@ -14,7 +14,6 @@ class SensoredMotorDriver(MotorDriver):
         # Properties
         self.PPR = 12  # pulses per revolution, PPR * 4 = CPR
         self.GEAR_RATIO = 46.8512  # speed reduction rate, v_wheel = v_motor / GEAR_RATIO
-        # self.WHEEL_RADIUS = 0.032  # diameter in mm -> radius in m  
     
     def update_pulses(self, pin):
         if self.encb_pin.value() == self.enca_pin.value():  # A channel RISE later than B channel
@@ -40,8 +39,6 @@ if __name__ == '__main__':
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
         ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
         print(f"wheel angular velocity: {ang_vel_whl} rad/s")
-        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
-        # print(f"wheel linear velocity: {lin_vel} m/s")        
     for d in reversed(range(200)):  # ramp down
         m.forward(int(65025 / 200 * d))
         sleep(0.02)
@@ -52,8 +49,6 @@ if __name__ == '__main__':
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
         ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
         print(f"wheel angular velocity: {ang_vel_whl} rad/s")
-        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
-        # print(f"wheel linear velocity: {lin_vel} m/s")
     # Reverse
     for d in range(200):  # ramp up
         m.backward(int(65025 / 200 * d))
@@ -64,9 +59,7 @@ if __name__ == '__main__':
         rads_inc = revs_inc * 2 * pi
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
         ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
-        print(f"wheel angular velocity: {ang_vel_whl} rad/s")
-        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
-        # print(f"wheel linear velocity: {lin_vel} m/s")        
+        print(f"wheel angular velocity: {ang_vel_whl} rad/s")      
     for d in reversed(range(200)):  # ramp down
         m.backward(int(65025 / 200 * d))
         sleep(0.02)
@@ -77,8 +70,6 @@ if __name__ == '__main__':
         ang_vel_mtr = rads_inc / 0.02  # motor angular velocity
         ang_vel_whl = ang_vel_mtr / m.GEAR_RATIO  # wheel angular velocity
         print(f"wheel angular velocity: {ang_vel_whl} rad/s")
-        # lin_vel = ang_vel_wheel * m.WHEEL_RADIUS
-        # print(f"wheel linear velocity: {lin_vel} m/s")
 
     m.stop()
     
