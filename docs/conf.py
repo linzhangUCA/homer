16b7aa11ce8cfb8560a96e936f9111ddeeaa0402
linzhanguca <lzhang12@uca.edu>
2025-01-15 07:07:49 -0600

    add theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'HomeR'
-copyright = '2025, linzhanguca'
-author = 'linzhanguca'
+project = "HomeR"
+copyright = "2025, linzhanguca"
+author = "linzhanguca"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+html_logo = "images/homer_logo.png"
+html_theme_options = {"logo_only": False}
