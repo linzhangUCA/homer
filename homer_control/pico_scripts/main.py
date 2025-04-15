4a292a1cb1a7515c798385ea94b97b29b10c99ff
linzhanguca <lzhang12@uca.edu>
2025-04-15 13:59:12 -0500

    update pico main to avoid errors

--- homer_control/pico_scripts/main.py
+++ homer_control/pico_scripts/main.py
@@ -1,23 +1,34 @@
 """
 Rename this script to main.py, then upload to the pico board.
 """
+
 import sys
 import select
 from diff_drive_controller import DiffDriveController
+from machine import reset
 
 # SETUP
 # Instantiate robot
-homer = DiffDriveController(left_ids=((2, 3, 4), (20, 21)), right_ids=((6, 7, 8), (10, 11)))
+homer = DiffDriveController(
+    left_ids=((2, 3, 4), (20, 21)), right_ids=((6, 7, 8), (10, 11))
+)
 # Create a poll to receive messages from host machine
 cmd_vel_listener = select.poll()
 cmd_vel_listener.register(sys.stdin, select.POLLIN)
 event = cmd_vel_listener.poll()
+target_lin_vel, target_ang_vel = 0.0, 0.0
 
 # LOOP
-while True:
-    # print(homer.lin_vel, homer.ang_vel)  # transmit actual robot velocity to host machine
-    for msg, _ in event:
-        buffer = msg.readline().rstrip().split(',')
-        if len(buffer) == 2:
-            target_lin_vel, target_ang_vel = float(buffer[0]), float(buffer[1])
+try:
+    while True:
+        # print(homer.lin_vel, homer.ang_vel)  # transmit actual robot velocity to host machine
+        for msg, _ in event:
+            buffer = msg.readline().rstrip().split(",")
+            if len(buffer) == 2:
+                try:
+                    target_lin_vel, target_ang_vel = float(buffer[0]), float(buffer[1])
+                except ValueError:
+                    pass
             homer.set_vel(target_lin_vel, target_ang_vel)
+except:
+    reset()
